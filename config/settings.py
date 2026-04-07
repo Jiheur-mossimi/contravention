88b6aa4febb5d009db0890c0df5438905b1608c3
senda-cedrick <108199833+senda-cedrick@users.noreply.github.com>
2026-04-07 17:12:59 +0100

    ok fg

--- config/settings.py
+++ config/settings.py
@@ -78,8 +78,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'contrevation_db',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
